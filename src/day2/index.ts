ac368a336b615f6813eb940881c988bd3be80c0b
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-02 14:40:54 +0100

    solved day 1 part 2

--- src/day2/index.ts
+++ src/day2/index.ts
@@ -1,16 +1,3 @@
-function solvePartOne(input: string) {
-  const games = input.split('\n').slice(0, -1)
-
-  let sum = 0
-  games.forEach((game, index) => {
-    if (isGamePossible(game, { red: 12, green: 13, blue: 14 })) {
-      sum += index + 1
-    }
-  })
-
-  return sum
-}
-
 const COLORS = ['red', 'green', 'blue'] as const
 type Color = (typeof COLORS)[number]
 export type Bag = { [key in Color]: number }
@@ -39,5 +26,52 @@ export function isGamePossible(game: string, bag: Bag): boolean {
   return true
 }
 
+function solvePartOne(input: string) {
+  const games = input.split('\n').slice(0, -1)
+
+  let sum = 0
+  games.forEach((game, index) => {
+    if (isGamePossible(game, { red: 12, green: 13, blue: 14 })) {
+      sum += index + 1
+    }
+  })
+
+  return sum
+}
+
+function getMinimumSetPower(game: string): number {
+  const bag: Bag = { red: 0, green: 0, blue: 0 }
+  const rawGame = game.split(': ')[1]
+  const sets = rawGame.split('; ')
+  for (const set of sets) {
+    const cubes = set.split(', ')
+    for (const cube of cubes) {
+      const [amountStr, color] = cube.split(' ')
+      const amount = Number(amountStr)
+
+      if (isNaN(amount)) throw Error('invalid amount')
+      if (!isColor(color)) throw Error(`invalid color: ${color}`)
+
+      if (bag[color] < amount) {
+        bag[color] = amount
+      }
+    }
+  }
+
+  return bag.red * bag.green * bag.blue
+}
+
+function solvePartTwo(input: string) {
+  const games = input.split('\n').slice(0, -1)
+
+  let sum = 0
+  games.forEach((game) => {
+    sum += getMinimumSetPower(game)
+  })
+
+  return sum
+}
+
 const input = await Bun.file('./src/day2/input.txt').text()
 console.log(solvePartOne(input))
+console.log(solvePartTwo(input))
