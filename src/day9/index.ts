bd6544111bd74298bbf2611aec5e2f4ee15623a8
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-10 16:36:40 +0100

    day9: solve part two

--- src/day9/index.ts
+++ src/day9/index.ts
@@ -26,6 +26,34 @@ export function solvePartOne(input: string): number {
   return total
 }
 
+export function solvePartTwo(input: string): number {
+  const histories = input
+    .slice(0, -1)
+    .split('\n')
+    .map((line) => line.split(' ').map(Number))
+  let total = 0
+
+  histories.forEach((history) => {
+    const sequences = [history]
+    while (sequences.at(-1)?.some((v) => v !== 0)) {
+      const lastSequence = sequences.at(-1) as number[]
+      const newSequence = []
+      for (let i = 0; i < lastSequence.length - 1; i++) {
+        newSequence.push(lastSequence[i + 1] - lastSequence[i])
+      }
+      sequences.push(newSequence)
+    }
+
+    sequences.at(-1)?.unshift(0)
+    for (let i = sequences.length - 2; i >= 0; i--) {
+      sequences[i].unshift(sequences[i][0] - sequences[i + 1][0])
+    }
+    total += sequences[0][0]
+  })
+
+  return total
+}
+
 const input = await Bun.file('./src/day9/input.txt').text()
 console.log('Part one: ', solvePartOne(input))
-// console.log('Part two: ', solvePartTwo(input))
+console.log('Part two: ', solvePartTwo(input))
