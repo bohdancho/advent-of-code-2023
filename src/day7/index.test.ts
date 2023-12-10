02c62135d9e62899fcf88a9962187155a610ab71
bohdancho <bogdan.chornocondratenko@gmail.com>
2023-12-10 14:28:16 +0100

    day7: solve part two

--- src/day7/index.test.ts
+++ src/day7/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'bun:test'
-import { solvePartOne } from './index'
+import { solve } from './index'
 
 const input = `32T3K 765
 T55J5 684
@@ -8,5 +8,9 @@ KTJJT 220
 QQQJA 483\n`
 
 test('part one', () => {
-  expect(solvePartOne(input)).toBe(6440)
+  expect(solve(input)).toBe(6440)
+})
+
+test('part two', () => {
+  expect(solve(input, true)).toBe(5905)
 })
--- src/day7/index.ts
+++ src/day7/index.ts
@@ -1,4 +1,4 @@
-export function solvePartOne(input: string): number {
+export function solve(input: string, useWildCard = false): number {
   const hands = input
     .slice(0, -1)
     .split('\n')
@@ -8,11 +8,12 @@ export function solvePartOne(input: string): number {
     })
 
   hands.sort((a, b) => {
-    const combinationDifference = getCombinationStrength(a.cards) - getCombinationStrength(b.cards)
+    const combinationDifference =
+      getCombinationStrength(a.cards, useWildCard) - getCombinationStrength(b.cards, useWildCard)
     if (combinationDifference !== 0) {
       return combinationDifference
     }
-    return compareLabels(a.cards, b.cards)
+    return compareLabels(a.cards, b.cards, useWildCard)
   })
 
   let total = 0
@@ -20,12 +21,11 @@ export function solvePartOne(input: string): number {
   return total
 }
 
-const combinations = [[1, 1, 1, 1, 1], [2, 1, 1, 1], [2, 2, 1], [3, 1, 1], [3, 2], [4, 1], [5]].map((v) =>
+const combinations = [[1, 1, 1, 1, 1], [1, 1, 1, 2], [1, 2, 2], [1, 1, 3], [2, 3], [1, 4], [5]].map((v) =>
   JSON.stringify(v),
 )
-const labelsOrder = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
 
-function getCombinationStrength(cards: string): number {
+function getCombinationStrength(cards: string, useWildCard = false): number {
   const occurances: Record<string, number> = {}
   cards.split('').forEach((card) => {
     if (!occurances[card]) {
@@ -34,11 +34,22 @@ function getCombinationStrength(cards: string): number {
     }
     occurances[card] += 1
   })
-  const combination = Object.values(occurances).sort((a, b) => b - a)
+
+  let wildCardsAmount = 0
+  if (useWildCard && 'J' in occurances && occurances['J'] !== 5) {
+    wildCardsAmount = occurances['J']
+    delete occurances['J']
+  }
+
+  const combination = Object.values(occurances).sort((a, b) => a - b)
+  combination[combination.length - 1] += wildCardsAmount
   return combinations.indexOf(JSON.stringify(combination))
 }
 
-function compareLabels(a: string, b: string) {
+const labelsOrderNormal = ['A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2']
+const labelsOrderWildcard = ['A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J']
+function compareLabels(a: string, b: string, useWildCard = false) {
+  const labelsOrder = useWildCard ? labelsOrderWildcard : labelsOrderNormal
   for (let i = 0; i < a.length; i++) {
     if (a[i] !== b[i]) {
       return labelsOrder.indexOf(b[i]) - labelsOrder.indexOf(a[i])
@@ -48,5 +59,5 @@ function compareLabels(a: string, b: string) {
 }
 
 const input = await Bun.file('./src/day7/input.txt').text()
-console.log('Part one: ', solvePartOne(input))
-// console.log('Part two: ', solvePartTwo(input))
+console.log('Part one: ', solve(input))
+console.log('Part two: ', solve(input, true))
